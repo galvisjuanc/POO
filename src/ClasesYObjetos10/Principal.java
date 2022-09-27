a23eae84206a98af5ec5d198376dc3067fd17ae5
Juan <galvisjuanc@gmail.com>
2022-09-27 08:54:56 -0500

    Herencia Extends in POO

---
+++ src/ClasesYObjetos10/Principal.java
@@ -0,0 +1,9 @@
+package ClasesYObjetos10;
+
+public class Principal {
+    public static void main(String[] args) {
+        Estudiante estudiante = new Estudiante("Juan", "Galvis", 30, 123, 15.6f);
+
+        estudiante.mostrarDatos();
+    }
+}
