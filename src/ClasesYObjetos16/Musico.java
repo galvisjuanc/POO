357e5a3363a208d97e518a2455119d41a5294025
Juan <galvisjuanc@gmail.com>
2022-10-03 09:59:22 -0500

    Interfaces en Java - Herencia Múltiple

---
+++ src/ClasesYObjetos16/Musico.java
@@ -0,0 +1,6 @@
+package ClasesYObjetos16;
+
+public interface Musico extends Persona{
+
+    public abstract void tocarMusica();
+}
---
+++ src/ClasesYObjetos16/MusicoEstudiante.java
@@ -0,0 +1,19 @@
+package ClasesYObjetos16;
+
+public class MusicoEstudiante implements Musico, Estudiante{
+
+    @Override
+    public void hablar() {
+        System.out.println("Estoy hablando español");
+    }
+
+    @Override
+    public void tocarMusica() {
+        System.out.println("Estoy tocando la guitarra");
+    }
+
+    @Override
+    public void estudiar() {
+        System.out.println("Estudiando programación");
+    }
+}
